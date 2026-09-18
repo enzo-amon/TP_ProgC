995e701d1dbdb4716d1b4b913d6550e2e9571c58
enzo-amon <enzo.nino.amon@gmail.com>
2026-09-18 09:07:17 +0000

    Exercice 1.5 operateurs

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -1,2 +1,20 @@
+#include <stdio.h>
 
+int main() {
+    int a = 16;
+    int b = 3;
+
+    printf("a = %d, b = %d\n\n", a, b);
+
+    printf("a + b = %d\n", a + b);        // 19
+    printf("a - b = %d\n", a - b);        // 13
+    printf("a * b = %d\n", a * b);        // 48
+    printf("a / b = %d\n", a / b);        // 5  (division entière)
+    printf("a %% b = %d\n", a % b);       // 1  (reste)
+
+    printf("(a == b) = %d\n", a == b);    // 0  (faux)
+    printf("(a > b)  = %d\n", a > b);     // 1  (vrai)
+
+    return 0;
+}
 
