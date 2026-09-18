b30bb2b8b09874a8d35634b6471ff46271830b7a
enzo-amon <enzo.nino.amon@gmail.com>
2026-09-18 09:26:43 +0000

    Exercice 1.10 pyramide

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
 
+int main() {
+    int n = 5;   // hauteur de la pyramide
+    int i, j;
+
+    for (i = 1; i <= n; i++) {           // chaque niveau de la pyramide
+
+        // 1) les espaces pour centrer : n - i espaces
+        for (j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+
+        // 2) partie montante : 1, 2, ..., i
+        for (j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+
+        // 3) partie descendante : i-1, i-2, ..., 1
+        for (j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+
+        printf("\n");                    // passage a la ligne suivante
+    }
+
+    printf("Generation de la pyramide terminee.\n");
+
+    return 0;
+}
